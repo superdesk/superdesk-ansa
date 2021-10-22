c019022cbf49f3e665830304b61275cb95425c82
Petr Jašek <jasekpetr@gmail.com>
2021-10-22 15:14:42 +0200

    Initialize ansa-updated

---
+++ client/index.js
@@ -0,0 +1,37 @@
+import {startApp} from 'superdesk-core/scripts/index';
+import ansaModule from './ansa';
+
+setTimeout(() => startApp(
+    [
+        {
+            id: 'ansaIptc',
+            load: () => import('./extensions/ansaIptc'),
+        },
+        {
+            id: 'imageShortcuts',
+            load: () => import('./extensions/imageShortcuts'),
+        },
+
+        {
+            id: 'ansa-archive',
+            load: () => import('./extensions/ansa-archive'),
+        },
+        {
+            id: 'lineCountInAuthoringHeader',
+            load: () => import('./extensions/lineCountInAuthoringHeader'),
+        },
+        {
+            id: 'planning-extension',
+            load: () => import('superdesk-planning/client/planning-extension'),
+        },
+    ],
+    {},
+    {
+        countLines: (plainText, lineLength) =>
+            plainText
+                .split('\n')
+                .reduce((sum, line) => sum + (line.length > 0 ? Math.ceil(line.length / lineLength) : 1), 0),
+    },
+));
+
+export default ansaModule;
