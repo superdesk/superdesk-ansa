8429269f5d93773f3396ef35c3fb9b523934c4c8
Ivan Jelic <ivan.jelic@sourcefabric.org>
2026-04-21 18:56:56 +0200

    Fix

--- client/extensions/lineCountInAuthoringHeader/src/line-count-toolbar-widget.tsx
+++ client/extensions/lineCountInAuthoringHeader/src/line-count-toolbar-widget.tsx
@@ -2,42 +2,19 @@ import * as React from 'react';
 
 import {IArticle, ISuperdesk} from 'superdesk-api';
 
-const POLL_INTERVAL_MS = 1000;
-
 export function getLineCountToolbarWidget(superdesk: ISuperdesk) {
     const {gettext, gettextPlural} = superdesk.localization;
     const {getLinesCount, stripHtmlTags} = superdesk.utilities;
 
-    type IProps = {entity: IArticle};
-    type IState = {linesCount: number | null};
-
-    return class LineCountToolbarWidget extends React.PureComponent<IProps, IState> {
-        private pollTimer: number | null = null;
-
-        constructor(props: IProps) {
-            super(props);
-            this.state = {linesCount: computeLinesCount(props.entity)};
-        }
-
-        componentDidMount() {
-            this.pollTimer = window.setInterval(this.tick, POLL_INTERVAL_MS);
-        }
-
-        componentWillUnmount() {
-            if (this.pollTimer != null) {
-                window.clearInterval(this.pollTimer);
-            }
-        }
-
-        private tick = () => {
-            const next = computeLinesCount(this.props.entity);
-            if (next !== this.state.linesCount) {
-                this.setState({linesCount: next});
-            }
-        };
-
+    return class LineCountToolbarWidget extends React.PureComponent<{entity: IArticle}> {
         render() {
-            const {linesCount} = this.state;
+            const {entity} = this.props;
+
+            if (entity.body_html == null) {
+                return null;
+            }
+
+            const linesCount = getLinesCount(stripHtmlTags(entity.body_html));
 
             if (linesCount == null) {
                 return null;
@@ -52,11 +29,4 @@ export function getLineCountToolbarWidget(superdesk: ISuperdesk) {
             );
         }
     };
-
-    function computeLinesCount(entity: IArticle): number | null {
-        if (entity.body_html == null) {
-            return null;
-        }
-        return getLinesCount(stripHtmlTags(entity.body_html));
-    }
 }
