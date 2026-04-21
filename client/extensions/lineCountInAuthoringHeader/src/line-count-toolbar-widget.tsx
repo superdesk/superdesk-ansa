3e88353923687a1c97d50bdc4a9557232469f9ef
Ivan Jelic <ivan.jelic@sourcefabric.org>
2026-04-21 19:43:13 +0200

    Update

--- client/extensions/lineCountInAuthoringHeader/src/line-count-toolbar-widget.tsx
+++ client/extensions/lineCountInAuthoringHeader/src/line-count-toolbar-widget.tsx
@@ -6,16 +6,69 @@ export function getLineCountToolbarWidget(superdesk: ISuperdesk) {
     const {gettext, gettextPlural} = superdesk.localization;
     const {getLinesCount, stripHtmlTags} = superdesk.utilities;
 
-    return class LineCountToolbarWidget extends React.PureComponent<{entity: IArticle}> {
-        render() {
-            const {entity} = this.props;
-            const plainText = getPlainTextForLineCount(entity);
+    return class LineCountToolbarWidget extends React.PureComponent<
+        {entity: IArticle},
+        {linesCount: number | null}
+    > {
+        private observer: MutationObserver | null = null;
+        private editorEl: HTMLElement | null = null;
+        private attachTimer: number | null = null;
 
-            if (plainText == null) {
-                return null;
+        constructor(props: {entity: IArticle}) {
+            super(props);
+            this.state = {linesCount: computeFromEntity(props.entity)};
+        }
+
+        componentDidMount() {
+            this.tryAttachObserver();
+        }
+
+        componentDidUpdate(prevProps: {entity: IArticle}) {
+            if (this.editorEl == null && prevProps.entity !== this.props.entity) {
+                const next = computeFromEntity(this.props.entity);
+                if (next !== this.state.linesCount) {
+                    this.setState({linesCount: next});
+                }
+            }
+        }
+
+        componentWillUnmount() {
+            this.observer?.disconnect();
+            if (this.attachTimer != null) {
+                window.clearTimeout(this.attachTimer);
+            }
+        }
+
+        private tryAttachObserver = () => {
+            const el = document.getElementById('bodyhtml');
+
+            if (el == null) {
+                this.attachTimer = window.setTimeout(this.tryAttachObserver, 250);
+                return;
             }
 
-            const linesCount = getLinesCount(plainText);
+            this.editorEl = el;
+            this.observer = new MutationObserver(this.recompute);
+            this.observer.observe(el, {
+                characterData: true,
+                subtree: true,
+                childList: true,
+            });
+            this.recompute();
+        };
+
+        private recompute = () => {
+            if (this.editorEl == null) {
+                return;
+            }
+            const next = getLinesCount(this.editorEl.innerText ?? '');
+            if (next !== this.state.linesCount) {
+                this.setState({linesCount: next});
+            }
+        };
+
+        render() {
+            const {linesCount} = this.state;
 
             if (linesCount == null) {
                 return null;
@@ -31,15 +84,15 @@ export function getLineCountToolbarWidget(superdesk: ISuperdesk) {
         }
     };
 
-    function getPlainTextForLineCount(entity: IArticle): string | null {
+    function computeFromEntity(entity: IArticle): number | null {
         const blocks = entity.fields_meta?.body_html?.draftjsState?.[0]?.blocks;
 
         if (blocks != null) {
-            return blocks.map((b: {text: string}) => b.text).join('\n');
+            return getLinesCount(blocks.map((b: {text: string}) => b.text).join('\n'));
         }
 
         if (entity.body_html != null) {
-            return stripHtmlTags(entity.body_html);
+            return getLinesCount(stripHtmlTags(entity.body_html));
         }
 
         return null;
