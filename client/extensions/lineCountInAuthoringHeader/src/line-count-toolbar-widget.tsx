da28688664f7eaa5aedc7d343590bd72f24b4710
Ivan Jelic <ivan.jelic@sourcefabric.org>
2026-04-21 17:38:24 +0200

    Update line count in the authoring toolbar

--- client/extensions/lineCountInAuthoringHeader/src/line-count-toolbar-widget.tsx
+++ client/extensions/lineCountInAuthoringHeader/src/line-count-toolbar-widget.tsx
@@ -2,19 +2,42 @@ import * as React from 'react';
 
 import {IArticle, ISuperdesk} from 'superdesk-api';
 
+const POLL_INTERVAL_MS = 1000;
+
 export function getLineCountToolbarWidget(superdesk: ISuperdesk) {
     const {gettext, gettextPlural} = superdesk.localization;
     const {getLinesCount, stripHtmlTags} = superdesk.utilities;
 
-    return class LineCountToolbarWidget extends React.PureComponent<{entity: IArticle}> {
-        render() {
-            const {entity} = this.props;
+    type IProps = {entity: IArticle};
+    type IState = {linesCount: number | null};
 
-            if (entity.body_html == null) {
-                return null;
+    return class LineCountToolbarWidget extends React.PureComponent<IProps, IState> {
+        private pollTimer: number | null = null;
+
+        constructor(props: IProps) {
+            super(props);
+            this.state = {linesCount: computeLinesCount(props.entity)};
+        }
+
+        componentDidMount() {
+            this.pollTimer = window.setInterval(this.tick, POLL_INTERVAL_MS);
+        }
+
+        componentWillUnmount() {
+            if (this.pollTimer != null) {
+                window.clearInterval(this.pollTimer);
             }
+        }
 
-            const linesCount = getLinesCount(stripHtmlTags(entity.body_html));
+        private tick = () => {
+            const next = computeLinesCount(this.props.entity);
+            if (next !== this.state.linesCount) {
+                this.setState({linesCount: next});
+            }
+        };
+
+        render() {
+            const {linesCount} = this.state;
 
             if (linesCount == null) {
                 return null;
@@ -28,5 +51,12 @@ export function getLineCountToolbarWidget(superdesk: ISuperdesk) {
                 </dl>
             );
         }
+    };
+
+    function computeLinesCount(entity: IArticle): number | null {
+        if (entity.body_html == null) {
+            return null;
+        }
+        return getLinesCount(stripHtmlTags(entity.body_html));
     }
 }
