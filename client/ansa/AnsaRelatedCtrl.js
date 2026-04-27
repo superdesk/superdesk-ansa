13ba348f32134d4f36a5f45d09d37ebf92c658f6
Ivan Jelic <ivan.jelic@sourcefabric.org>
2026-04-27 12:54:40 +0200

    Disable Semantics

--- client/ansa/AnsaRelatedCtrl.js
+++ client/ansa/AnsaRelatedCtrl.js
@@ -1,5 +1,3 @@
-import {isEmpty} from 'lodash';
-import { refreshAnalysis } from '.';
 // duplicated in extensions/imageShortcuts/src/get-widgets.tsx
 // constants aren't used to avoid imports between multiple projects
 const featureMediaField = 'feature_media';
@@ -12,137 +10,100 @@ const getQueryTimeZone = () => {
     return now.getTimezoneOffset() === -120 ? '+02:00' : '+01:00';
 };
 
-AnsaRelatedCtrl.$inject = ['$scope', 'api', '$rootScope', 'storage', 'Keys'];
-export default function AnsaRelatedCtrl($scope, api, $rootScope, storage, Keys) {
-    refreshAnalysis($scope, api, $rootScope, false).then(() => {
-        const search = () => {
-            this.items = null;
-            let semantics = $scope.item.semantics || {};
+AnsaRelatedCtrl.$inject = ['$scope', 'api', 'storage', 'Keys'];
+export default function AnsaRelatedCtrl($scope, api, storage, Keys) {
+    const search = () => {
+        if (!this.query) {
+            this.items = [];
+            this.searched = false;
+            return;
+        }
 
-            if (isEmpty(semantics.iptcCodes) && isEmpty(semantics.persons)
-                && isEmpty(semantics.organisations) && !this.query) {
-                this.items = [];
-                return;
-            }
+        this.items = null;
+        this.searched = true;
 
-            let filters = [];
-            let keys = ['persons', 'organizations'];
-            let namespace = (key) => 'semantics.' + key;
-
-            keys.forEach((key) => {
-                if (semantics[key] && semantics[key].length) {
-                    semantics[key].forEach((val) => {
-                        let f = {};
-
-                        f[namespace(key)] = val;
-
-                        if (key === 'persons') {
-                            filters.push({match: {[namespace(key)]: {query: val, operator: 'and'}}});
-                        } else {
-                            filters.push({match_phrase: f});
-                        }
-                    });
-                }
-            });
-
-            let query = {
-                bool: {
-                    must: [{term: {type: this.activeFilter}}],
-                    should: filters,
-                    must_not: [{term: {item_id: $scope.item._id}}],
-                    minimum_should_match: window.MINIMUM_SHOULD_MATCH || 1,
-                },
-            };
-
-            if (!isEmpty(semantics.iptcCodes) && this.activeFilter === 'text') {
-                query.bool.must.push({terms: {'semantics.iptcCodes': semantics.iptcCodes}});
-            }
-
-            if (this.query) {
-                query = {
-                    bool: {
-                        must: [
-                            {term: {type: this.activeFilter}},
-                            {query_string: {query: this.query, lenient: true, default_operator: 'AND'}},
-                        ],
-                        must_not: [{term: {item_id: $scope.item._id}}],
-                    },
-                };
-            }
-
-            // filter out older versions
-            query.bool.must.push(
-                {term: {state: 'published'}}
-            );
-
-            // and rewritten versions
-            query.bool.must_not.push(
-                {exists: {field: 'rewritten_by'}}
-            );
-
-            // filter out afp
-            query.bool.must_not.push({term: {creditline: 'AFP'}});
-
-            // filter out used
-            query.bool.must_not.push({range: {used_updated: {gte: 'now/d', time_zone: getQueryTimeZone()}}});
-
-            console.info('query', angular.toJson(query, 2));
-
-            this.apiQuery(query);
+        const query = {
+            bool: {
+                must: [
+                    {term: {type: this.activeFilter}},
+                    {query_string: {query: this.query, lenient: true, default_operator: 'AND'}},
+                ],
+                must_not: [{term: {item_id: $scope.item._id}}],
+            },
         };
 
-        this.activeFilter = storage.getItem('ansa.related.filter') || 'text';
+        // filter out older versions
+        query.bool.must.push(
+            {term: {state: 'published'}}
+        );
 
-        this.filter = (activeFilter) => {
-            this.activeFilter = activeFilter;
-            storage.setItem('ansa.related.filter', activeFilter);
-            search();
-        };
+        // and rewritten versions
+        query.bool.must_not.push(
+            {exists: {field: 'rewritten_by'}}
+        );
 
-        this.searchOnEnter = (event) => {
-            if (event.which === Keys.enter) {
-                search();
-            }
-        };
+        // filter out afp
+        query.bool.must_not.push({term: {creditline: 'AFP'}});
 
-        this.apiQuery = (query) => api.query('news', {source: {
-            query: query,
-            sort: [{versioncreated: 'desc'}],
-            size: 50,
-        }})
-            .then((response) => {
-                this.items = response._items.map((published) => Object.assign(
-                    {},
-                    published.archive_item || published,
-                    {_type: published.archive_item ? 'archive' : 'published'},
-                ));
-            }, (reason) => {
-                this.items = [];
-            });
+        // filter out used
+        query.bool.must_not.push({range: {used_updated: {gte: 'now/d', time_zone: getQueryTimeZone()}}});
 
+        console.info('query', angular.toJson(query, 2));
+
+        this.apiQuery(query);
+    };
+
+    this.items = [];
+    this.searched = false;
+    this.activeFilter = storage.getItem('ansa.related.filter') || 'text';
+
+    this.filter = (activeFilter) => {
+        this.activeFilter = activeFilter;
+        storage.setItem('ansa.related.filter', activeFilter);
         search();
+    };
 
-        // set given picture as featured for current item
-        this.setFeatured = (picture) => {
-            window['__private_ansa__add_image_to_article'](featureMediaField, picture);
-        };
+    this.searchOnEnter = (event) => {
+        if (event.which === Keys.enter) {
+            search();
+        }
+    };
 
-        // add picture to item photo gallery
-        this.addToGallery = (picture) => {
-            window['__private_ansa__add_image_to_article'](galleryField, picture);
-        };
-
-        this.allowAddMedia = () => $scope._editable && $scope.item.type === 'text';
-
-        $scope.featureMediaField = featureMediaField;
-        $scope.galleryField = galleryField;
-
-        $scope.$watch('item', (item) => {
-            if (item != null) {
-                $scope.contentProfile = window['__private_ansa__get_content_profile'](item.profile) || null;
-            } else {
-                $scope.contentProfile = null;
-            }
+    this.apiQuery = (query) => api.query('news', {source: {
+        query: query,
+        sort: [{versioncreated: 'desc'}],
+        size: 50,
+    }})
+        .then((response) => {
+            this.items = response._items.map((published) => Object.assign(
+                {},
+                published.archive_item || published,
+                {_type: published.archive_item ? 'archive' : 'published'},
+            ));
+        }, (reason) => {
+            this.items = [];
         });
+
+    // set given picture as featured for current item
+    this.setFeatured = (picture) => {
+        window['__private_ansa__add_image_to_article'](featureMediaField, picture);
+    };
+
+    // add picture to item photo gallery
+    this.addToGallery = (picture) => {
+        window['__private_ansa__add_image_to_article'](galleryField, picture);
+    };
+
+    this.allowAddMedia = () => $scope._editable && $scope.item.type === 'text';
+
+    $scope.featureMediaField = featureMediaField;
+    $scope.galleryField = galleryField;
+
+    $scope.$watch('item', (item) => {
+        if (item != null) {
+            $scope.contentProfile = window['__private_ansa__get_content_profile'](item.profile) || null;
+        } else {
+            $scope.contentProfile = null;
+        }
     });
 }
